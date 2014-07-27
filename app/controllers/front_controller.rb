a0b9d7710830c265a87bd45b54c1a575a46220b8
cjshort <callumshorty@hotmail.com>
2014-07-27 22:48:52 +0100

    Initial

---
+++ app/controllers/front_controller.rb
@@ -0,0 +1,13 @@
+class FrontController < ApplicationController
+  def index
+  end
+
+  def howitworks
+  end
+
+  def blog
+  end
+
+  def connect
+  end
+end
---
+++ app/controllers/panel_controller.rb
@@ -0,0 +1,15 @@
+class PanelController < ApplicationController
+	layout 'panel'
+
+  def pageedit
+  end
+
+  def requestchange
+  end
+
+  def statistics
+  end
+
+  def payment
+  end
+end
