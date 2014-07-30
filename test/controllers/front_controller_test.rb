c9314171b06c3148243a1ec1d76e33bd841ec7ba
cjshort <callumshorty@hotmail.com>
2014-07-30 23:05:01 +0100

    db fixtures fix #3

--- test/controllers/front_controller_test.rb
+++ test/controllers/front_controller_test.rb
@@ -1,24 +1,5 @@
 require 'test_helper'
 
 class FrontControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get howitworks" do
-    get :howitworks
-    assert_response :success
-  end
-
-  test "should get blog" do
-    get :blog
-    assert_response :success
-  end
-
-  test "should get connect" do
-    get :connect
-    assert_response :success
-  end
 
 end
--- test/controllers/panel_controller_test.rb
+++ test/controllers/panel_controller_test.rb
@@ -1,24 +1,5 @@
 require 'test_helper'
 
 class PanelControllerTest < ActionController::TestCase
-  test "should get pageedit" do
-    get :pageedit
-    assert_response :success
-  end
-
-  test "should get requestchange" do
-    get :requestchange
-    assert_response :success
-  end
-
-  test "should get statistics" do
-    get :statistics
-    assert_response :success
-  end
-
-  test "should get payment" do
-    get :payment
-    assert_response :success
-  end
 
 end
