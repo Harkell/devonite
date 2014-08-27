e955934c6adfe720dbf5928847f9699257131303
J <comely@hotmail.co.uk>
2014-08-27 22:36:19 +0100

    Changed connect to support (JHS)

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -14,6 +14,6 @@ class FrontController < ApplicationController
     end
   end
 
-  def connect
+  def support
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   get 'front/blog'
 
-  get 'front/connect'
+  get 'front/support'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
