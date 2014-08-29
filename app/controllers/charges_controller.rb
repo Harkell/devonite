91eaaab5c3dc701250248362ae14853253c6d9e1
J <comely@hotmail.co.uk>
2014-08-29 23:35:24 +0100

    Finished stripe payment system (JHS)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,19 +6,21 @@ end
 
 def create
   # Amount in pence?
-  @amount = 5000
+  #@amount = 5000
 
   customer = Stripe::Customer.create(
     :email => 'example@stripe.com',
-    :card  => params[:stripeToken]
+    :card  => params[:stripeToken],
+    :plan  => 'dvnt-test'
   )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'gbp'
-  )
+ # charge = Stripe::Charge.create(
+ #  :customer    => customer.id,
+ #   :amount      => @amount,
+ #   :description => 'Rails Stripe customer',
+ #   :currency    => 'gbp'
+ # )
+  Stripe::Plan.retrieve("dvnt-test")
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
