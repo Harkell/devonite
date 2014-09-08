ef321cca14f337776ff33a73580c5f0c50dd880c
J Short <comely@hotmail.co.uk>
2014-09-08 17:00:04 +0100

    Stripe Hooks test (JHS)

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,11 @@ Rails.application.routes.draw do
 
   resources "requests", only: [:new, :create]
 
+
+  scope '/hooks', :controller => :hooks do
+  post :receiver
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
