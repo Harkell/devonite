09277c2f6bf1a52eaaa4d132821c84215b8f20cf
J <comely@hotmail.co.uk>
2014-09-09 21:10:52 +0100

    heroku test

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   resources "requests", only: [:new, :create]
 
 
-  scope '/hooks', :controller => :hooks_controller do
+  scope '/hooks', :controller => :hooks do
   post :receiver
 end
 
