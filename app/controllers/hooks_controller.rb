4121f348a12eb6ebc05234c2d5db71514a32fc23
J <comely@hotmail.co.uk>
2014-09-09 20:17:20 +0100

    test webhook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,7 +3,7 @@ class HooksController < ApplicationController
 
   #Stripe.api_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   #Stripe.api_key = Rails.configuration.stripe[:secret_key]
-  Stripe::api_key = ENV['SECRET_KEY']
+  #Stripe::api_key = ENV['SECRET_KEY']
 
   def receiver
 
