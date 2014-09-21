cf4f53dfc04710c6b2f712804e83091e326d790e
cjshort <callumshorty@hotmail.com>
2014-09-21 10:25:15 +0100

    content merge fix, charge styling, home page styling, readme update

--- db/migrate/20140827190014_change_content_table.rb
+++ db/migrate/20140827190014_change_content_table.rb
@@ -1,14 +1,5 @@
 class ChangeContentTable < ActiveRecord::Migration
   def change
-  	rename_column :contents, :header1, :section1
- 	rename_column :contents, :header2, :section2
-	rename_column :contents, :section1header1, :section3
-	rename_column :contents, :section1paragraph1, :section4
-	rename_column :contents, :section2header1, :section5
-	rename_column :contents, :section2paragraph1, :section6
-	rename_column :contents, :section2paragraph2, :section7
-	rename_column :contents, :section3header1, :section8
-	rename_column :contents, :section3paragraph1, :section9
 	add_column :contents, :section10, :text
 	add_column :contents, :section11, :text
 	add_column :contents, :section12, :text
