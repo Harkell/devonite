bf63796f01d2d71541e1102d25dbb1f5db3f7350
cjshort <callumshorty@hotmail.com>
2014-09-27 20:53:34 +0100

    getstarted styling, user sign in checks

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,9 +1,9 @@
 class Proposal < MailForm::Base # try ActionMailer::Base?
   #attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :website
-  attribute :phone
-  attribute :message
+  attribute :name
+  attribute :business
+  attribute :mobile
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
